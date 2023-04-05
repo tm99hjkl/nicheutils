cfbb1f7366a4e5a0f09e98f590a8b72d8fea3bb8
Takumi Matsuura <tm99hjkl@gmail.com>
2023-04-05 19:53:50 +0900

    update cptm and cmdify

--- cmdify.sh
+++ cmdify.sh
@@ -4,6 +4,11 @@ if [[ -f $1 ]] && [[ $1 =~ .sh ]]; then
     install -m 755 $1 /usr/local/bin/${1%.sh}
   else
     echo "cmdify: command \`${1%.sh}\` already exists."
+    read -p "do you want to update? [y/n]: " yn
+    case $yn in
+      [yY]* ) sudo rm /usr/local/bin/${1%.sh}; install -m 755 $1 /usr/local/bin/${1%.sh};;
+      * ) ;;
+    esac
     exit 1
   fi
 else
--- cptm.sh
+++ cptm.sh
@@ -4,6 +4,12 @@ FILE="/mnt/c/Users/takumi matsuura/Downloads/"$1
 
 if [ -f "$1" ]; then
   echo "cptm: file '$1' already exists."
+  read -p "do you want to update $1? [y/n]: " yn
+  case $yn in
+    [yY]* ) rm $1; echo "updated"; cp "$FILE" .;;
+    [nN]* ) echo "don't delete";;
+    *) ;;
+  esac
 else
   cp "$FILE" .
 fi
