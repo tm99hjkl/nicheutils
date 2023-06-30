62459d77a96e501d78b5df91a55b731f595b98a1
Takumi Matsuura <tm99hjkl@gmail.com>
2023-06-30 11:38:56 +0900

    fix bak

--- bak.sh
+++ bak.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 files="$@"
+cp_flag=""
 for f in $files; do
     if [[ $f != *.bak ]]; then
-        cp -r $f.bak
+        if [[ -f $f ]]; then
+            cp_flag=""
+        elif [[ -d $f ]]; then
+            cp_flag="-r"
+        fi
+        cp $cp_flag $f $f.bak
     fi
 done
